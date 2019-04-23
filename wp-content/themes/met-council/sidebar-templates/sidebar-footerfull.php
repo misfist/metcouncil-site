2c19ed0294275153c0895737882cbe27663c4462
Pea <pea@misfist.com>
2019-04-23 16:43:24 -0400

    overrode WP column styles

---
+++ wp-content/themes/met-council/sidebar-templates/sidebar-footerfull.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Sidebar setup for footer full.
+ *
+ * @package understrap
+ * @subpackage metcouncil
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+$container = get_theme_mod( 'understrap_container_type' );
+
+?>
+
+<?php if ( is_active_sidebar( 'footerfull' ) ) : ?>
+
+	<!-- ******************* The Footer Full-width Widget Area ******************* -->
+
+	<div class="wrapper" id="wrapper-footer-full">
+
+		<div class="<?php echo esc_attr( $container ); ?>" id="footer-full-content" tabindex="-1">
+
+			<?php dynamic_sidebar( 'footerfull' ); ?>
+
+		</div>
+
+	</div><!-- #wrapper-footer-full -->
+
+<?php endif; ?>
